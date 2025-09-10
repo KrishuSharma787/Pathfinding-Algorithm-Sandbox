cae73380ccef7a047decb81816c74039181bbfb5
KrishuSharma787 <krishu.sharma787@gmail.com>
2025-09-11 01:22:57 +0530

    Update grid.py

--- pathfinder/grid.py
+++ pathfinder/grid.py
@@ -24,3 +24,31 @@ class Grid:
 def manhattan_distance(a, b):
     """Calculates the Manhattan distance between two points."""
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+class UnionFind:
+    """A data structure for tracking disjoint sets, used in maze generation."""
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1] * n
+
+    def find(self, i):
+        """Finds the root of the set containing element i, with path compression."""
+        if self.parent[i] == i:
+            return i
+        self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, i, j):
+        """Merges the sets containing elements i and j, using union by rank."""
+        root_i = self.find(i)
+        root_j = self.find(j)
+        if root_i != root_j:
+            if self.rank[root_i] > self.rank[root_j]:
+                self.parent[root_j] = root_i
+            elif self.rank[root_i] < self.rank[root_j]:
+                self.parent[root_i] = root_j
+            else:
+                self.parent[root_j] = root_i
+                self.rank[root_i] += 1
+            return True
+        return False
